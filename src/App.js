008f5f7798770c30bea588c0709921d786b5c7e8
Munquab Anwer <munquabr30@gmail.com>
2023-09-18 16:45:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const[datas,setDatas]=useState([])
+  const [searchQuery, setSearchQuery] = useState('');
+  
+  const api="https://dummyjson.com/users"
+  const fetchapi= async(url)=> {
+    try{
+    const res=await fetch(url)
+    if (!res.ok) {
+      throw new Error(`HTTP error! Status: ${res.status}`);
+    }
+    const data=await res.json()
+    setDatas(data.users)
+    //console.log(data)
+  }
+  catch (error) {
+    console.error('Error fetching data:', error);
+  }
+  }
+  useEffect(()=>{
+    fetchapi(api);
+  },[])
+
+  const filteredData = datas.filter((item) =>
+  item.firstName.toLowerCase().includes(searchQuery.toLowerCase())
+);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="justify-center items-center">
+      <h1 className="text-4xl font-bold">
+        Api Fetching
+      </h1>
+      <input type="text"  placeholder='Search' onChange={(e) => setSearchQuery(e.target.value)} />
+      {/* {
+         datas.map((item) => (
+         <h2>{item.firstName}</h2>
+        )
+        )
+      } */}
+       {filteredData.map((item) => (
+        <h2 key={item.id}>{item.firstName}</h2>
+      ))}
     </div>
   );
 }
